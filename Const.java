4832ebca979528bd345d0abe552b3b8aed7ab124
BLu <bbnlkc@gmail.com>
2019-08-06 19:04:54 +0200

    Libgdx project - desktop only

--- Const.java
+++ Const.java
@@ -1,7 +1,7 @@
 package net.ltslab.games.breakoutgdx;
 
-    public class Const {
+public class Const {
     public static final int CAMERA_WIDTH = 480;
-        public static final int CAMERA_HEIGHT = 800;
-        public static final int NUMBER_OF_BRICKS = 30;
+    public static final int CAMERA_HEIGHT = 800;
+    public static final int NUMBER_OF_BRICKS = 30;
 }
---
+++ desktop/src/net/ltslab/games/breakoutgdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.ltslab.games.breakoutgdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.ltslab.games.breakoutgdx.BreakoutRLGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BreakoutRLGame(), config);
+	}
+}
